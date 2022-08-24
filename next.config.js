9a6df094696ac6c7771b1a6c38dd22305c83d042
ohprettyhak <haklee.googl@gmail.com>
2022-08-24 17:37:35 +0900

    feat: redirect posts index address to index page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/posts',
+        destination: '/page',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
