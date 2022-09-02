1ab70493f2fcabd4486f562c3eea54b18acfc338
ohprettyhak <haklee.googl@gmail.com>
2022-09-02 14:43:45 +0900

    feat: change 404 page button

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Box, Text, Button } from '@chakra-ui/react';
 import { FaRegFlushed } from 'react-icons/fa';
 
 import Layout from '@/components/layout';
 
 export default function NotFoundPage() {
+  const router = useRouter();
+
   return (
     <Layout title="404">
       <Box my={24}>
@@ -15,11 +18,9 @@ export default function NotFoundPage() {
           잘못된 주소이거나, 비공개된 페이지입니다.
         </Text>
         <Box mt={8} display="flex" justifyContent="center">
-          <Link href="/" passHref>
-            <Button as="a" borderRadius="8px">
-              ← 돌아가기
-            </Button>
-          </Link>
+          <Button onClick={() => router.back()} borderRadius="8px" cursor="pointer">
+            ← 돌아가기
+          </Button>
         </Box>
       </Box>
     </Layout>
